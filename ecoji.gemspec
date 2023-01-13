d9a594682873a6762c081fdaa16ecb604f6ec01c
TSUYUSATO Kitsune <make.just.on@gmail.com>
2023-01-13 18:17:41 +0900

    Introduce Rubocop

--- ecoji.gemspec
+++ ecoji.gemspec
@@ -1,36 +1,43 @@
+# frozen_string_literal: true
 
-lib = File.expand_path("../lib", __FILE__)
+lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "ecoji/version"
+require 'ecoji/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "ecoji"
+  spec.name          = 'ecoji'
   spec.version       = Ecoji::VERSION
-  spec.authors       = ["TSUYUSATO Kitsune"]
-  spec.email         = ["make.just.on@gmail.com"]
+  spec.authors       = ['TSUYUSATO Kitsune']
+  spec.email         = ['make.just.on@gmail.com']
 
-  spec.summary       = %q{Ecoji implementation in Ruby}
-  spec.description   = %q{Ecoji is a data-to-emoji encoding scheme. This library provides the implementation of Ecoji in Ruby.}
-  spec.homepage      = "https://github.com/makenowjust/ecoji.rb/"
-  spec.license       = "MIT"
+  spec.summary       = 'Ecoji implementation in Ruby'
+  spec.description   = 'Ecoji is a data-to-emoji encoding scheme. This library provides the implementation of Ecoji in Ruby.'
+  spec.homepage      = 'https://github.com/makenowjust/ecoji.rb/'
+  spec.license       = 'MIT'
 
   if spec.respond_to?(:metadata)
-    spec.metadata["homepage_uri"] = spec.homepage
-    spec.metadata["source_code_uri"] = "https://github.com/makenowjust/ecoji.rb/"
-    spec.metadata["changelog_uri"] = "https://github.com/makenowjust/ecoji.rb/blob/main/CHANGELOG.md"
+    spec.metadata['homepage_uri'] = spec.homepage
+    spec.metadata['source_code_uri'] = 'https://github.com/makenowjust/ecoji.rb/'
+    spec.metadata['changelog_uri'] = 'https://github.com/makenowjust/ecoji.rb/blob/main/CHANGELOG.md'
   else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+    raise 'RubyGems 2.0 or newer is required to protect against ' \
+          'public gem pushes.'
   end
 
-  spec.files         = Dir.chdir(File.expand_path('..', __FILE__)) do
+  spec.files = Dir.chdir(File.expand_path(__dir__)) do
     `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(dep|test)/}) }
   end
-  spec.bindir        = "exe"
+  spec.bindir        = 'exe'
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
+  spec.required_ruby_version = '3.2'
 
-  spec.add_development_dependency "bundler", "~> 2.4"
-  spec.add_development_dependency "rake", "~> 13.0"
-  spec.add_development_dependency "minitest", "~> 5.17"
+  spec.metadata['rubygems_mfa_required'] = 'true'
+
+  spec.add_development_dependency 'bundler', '~> 2.4'
+  spec.add_development_dependency 'minitest', '~> 5.17'
+  spec.add_development_dependency 'rake', '~> 13.0'
+  spec.add_development_dependency 'rubocop', '~> 1.43'
+  spec.add_development_dependency 'rubocop-minitest', '~> 0.25'
+  spec.add_development_dependency 'rubocop-rake', '~> 0.6'
 end
--- lib/ecoji.rb
+++ lib/ecoji.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 module Ecoji
 end
 
 require 'ecoji/emojis'
-require "ecoji/version"
+require 'ecoji/version'
 
 module Ecoji
   class Error < StandardError; end
 
   PADDING = 0x2615
-  PADDING_LAST_V1 = [0x269C, 0x1F3CD, 0x1F4D1, 0x1F64B]
-  PADDING_LAST_V2 = [0x1F977,0x1F6FC,0x1F4D1,0x1F64B]
+  PADDING_LAST_V1 = [0x269C, 0x1F3CD, 0x1F4D1, 0x1F64B].freeze
+  PADDING_LAST_V2 = [0x1F977, 0x1F6FC, 0x1F4D1, 0x1F64B].freeze
 
   REV_EMOJIS = begin
     map = {}
 
     Emojis::V1.each_with_index do |n, ordinal|
-      map[n] = { ordinal:, version: 1, padding: :none}
+      map[n] = { ordinal:, version: 1, padding: :none }
     end
 
     Emojis::V2.each_with_index do |n, ordinal|
       if map[n]
         map[n][:version] = 3
       else
-        map[n] = { ordinal:, version: 2, padding: :none}
+        map[n] = { ordinal:, version: 2, padding: :none }
       end
     end
 
@@ -48,14 +50,14 @@ module Ecoji
       padding_last = PADDING_LAST_V2
       trim = true
     else
-      raise Error.new('Version must be either 1 or 2')
+      raise Error, 'Version must be either 1 or 2'
     end
 
     result = []
     data.bytes.each_slice(5) do |s|
       encode_five(s, emojis, padding_last, trim) { |n| result << n }
     end
-    result.pack("U*")
+    result.pack('U*')
   end
 
   def self.decode(data)
@@ -67,33 +69,32 @@ module Ecoji
       emojis, expected_version = read_four(chars, expected_version)
       break if emojis.empty?
 
-      bits = emojis[0][:ordinal] << 30 |
-        emojis[1][:ordinal] << 20 |
-        emojis[2][:ordinal] << 10 |
-        emojis[3][:ordinal]
+      bits = (emojis[0][:ordinal] << 30) |
+             (emojis[1][:ordinal] << 20) |
+             (emojis[2][:ordinal] << 10) |
+             emojis[3][:ordinal]
       out = [
         bits >> 32,
         0xff & (bits >> 24),
         0xff & (bits >> 16),
         0xff & (bits >> 8),
-        0xff & bits,
+        0xff & bits
       ]
 
-      case
-      when emojis[1][:padding] == :fill
+      if emojis[1][:padding] == :fill
         out = out[0...1]
-      when emojis[2][:padding] == :fill
+      elsif emojis[2][:padding] == :fill
         out = out[0...2]
-      when emojis[3][:padding] == :fill
+      elsif emojis[3][:padding] == :fill
         out = out[0...3]
-      when emojis[3][:padding] == :last
+      elsif emojis[3][:padding] == :last
         out = out[0...4]
       end
 
       result.concat(out)
     end
 
-    result.pack("C*").force_encoding(Encoding::UTF_8)
+    result.pack('C*').force_encoding(Encoding::UTF_8)
   end
 
   private_class_method def self.encode_five(s, emojis, padding_last, trim)
@@ -106,27 +107,25 @@ module Ecoji
         yield PADDING
       end
     when 2
-      bits = s[0] << 32 | s[1] << 24
+      bits = (s[0] << 32) | (s[1] << 24)
       yield emojis[bits >> 30]
       yield emojis[0x3ff & (bits >> 20)]
       yield PADDING
-      unless trim
-        yield PADDING
-      end
+      yield PADDING unless trim
     when 3
-      bits = s[0] << 32 | s[1] << 24 | s[2] << 16
+      bits = (s[0] << 32) | (s[1] << 24) | (s[2] << 16)
       yield emojis[bits >> 30]
       yield emojis[0x3ff & (bits >> 20)]
       yield emojis[0x3ff & (bits >> 10)]
       yield PADDING
     when 4
-      bits = s[0] << 32 | s[1] << 24 | s[2] << 16 | s[3] << 8
+      bits = (s[0] << 32) | (s[1] << 24) | (s[2] << 16) | (s[3] << 8)
       yield emojis[bits >> 30]
       yield emojis[0x3ff & (bits >> 20)]
       yield emojis[0x3ff & (bits >> 10)]
       yield padding_last[0x3 & (bits >> 8)]
     when 5
-      bits = s[0] << 32 | s[1] << 24 | s[2] << 16 | s[3] << 8 | s[4]
+      bits = (s[0] << 32) | (s[1] << 24) | (s[2] << 16) | (s[3] << 8) | s[4]
       yield emojis[bits >> 30]
       yield emojis[0x3ff & (bits >> 20)]
       yield emojis[0x3ff & (bits >> 10)]
@@ -143,46 +142,45 @@ module Ecoji
 
     while index < 4
       if chars.empty?
-        return [], expected_version if index == 0
-        if saw_padding && (expected_version == 3 || expected_version == 2)
-          return emojis, expected_version
-        else
-          raise Error.new("Unexpected end of data, input data size not multiple of 4")
-        end
+        return [], expected_version if index.zero?
+        return emojis, expected_version if saw_padding && [3, 2].include?(expected_version)
+
+        raise Error, 'Unexpected end of data, input data size not multiple of 4'
+
       end
-      
+
       c = chars.shift
-      next if c == "\n" || c == "\r"
+      next if ["\n", "\r"].include?(c)
 
       einfo = REV_EMOJIS[c.ord]
-      raise Error.new("Non Ecoji character seen: #{c.inspect}") unless einfo
+      raise Error, "Non Ecoji character seen: #{c.inspect}" unless einfo
 
       if einfo[:version] != 3
         if expected_version == 3
           expected_version = einfo[:version]
         elsif expected_version != einfo[:version]
-          raise Error.new("Emojis from different ecoji versions seen: #{c.inspect}")
+          raise Error, "Emojis from different ecoji versions seen: #{c.inspect}"
         end
       end
 
       case einfo[:padding]
       when :none
         if saw_padding
-          if expected_version == 3 || expected_version == 2
-            chars.unshift(c)
-            # NOTE(makenowjust): I think here we should set `expected_version` to `2`,
-            # but the original implementation does not.
-            # For the compatibility, `expected_version` is kept.
-            return emojis, expected_version
-          else
-            raise Error.new("Unexpectedly saw non-padding after padding")
-          end
+          raise Error, 'Unexpectedly saw non-padding after padding' unless [3, 2].include?(expected_version)
+
+          chars.unshift(c)
+          # NOTE(makenowjust): I think here we should set `expected_version` to `2`,
+          # but the original implementation does not.
+          # For the compatibility, `expected_version` is kept.
+          return emojis, expected_version
+
         end
       when :fill
-        raise Error.new("Padding unexpectedly seen in first position") if index == 0
+        raise Error, 'Padding unexpectedly seen in first position' if index.zero?
+
         saw_padding = true
       when :last
-        raise Error.new("Last padding seen in unexpected position") if index != 3
+        raise Error, 'Last padding seen in unexpected position' if index != 3
       end
 
       emojis[index] = einfo
@@ -190,5 +188,5 @@ module Ecoji
     end
 
     [emojis, expected_version]
-  end 
+  end
 end
